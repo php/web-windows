413792ea821be59cfa882a6f1f0a40897dd194c1
Pierre Joye <pajoye@php.net>
2009-03-11 10:50:05 +0000

    - 5.2.9-1 announce

--- docroot/index.php
+++ docroot/index.php
@@ -21,7 +21,20 @@ include TPL_PATH . 'left_column.php';
                         <div class="content">
                             
                             <div class="block"><!-- .block -->
-                                <span class="corners-top"><span></span></span>
+ 
+                               <div class="info entry"><!-- .info -->
+                                   <h3 class="summary entry-title">PHP 5.2.9-1 (Windows) released</h3>
+                                   <p class="news-date"><abbr class="published newsdate" title="2009-03-10T21:03:42+02:00">10-Mar-2009</abbr></p>
+                                   <div class="newsImage"></div>
+                                   <div>
+					<p>The PHP Development Team would like to announce the availability of a new Windows build of PHP - PHP 5.2.9-1</p>
+				     	<p>This release focuses on fixing a security flaw introduced by the cURL library (CVE-2009-0037). Please see the following for a full description: <a href="http://curl.haxx.se/docs/adv_20090303.html">http://curl.haxx.se/docs/adv_20090303.html</a></p>
+			     		<p>Please note that the cURL related function is disabled when open_basedir or safe_mode enabled.</p>
+		     			<p>Note: Only the Windows packages are affected.</p>
+					<p>Binaries are available in our <a href="/download/">downloads page</a></p>
+                                    </div>
+                               </div><!-- .info -->
+                               <span class="corners-top"><span></span></span>
 
                                 <div class="info entry"><!-- .info -->
                                     <h3 class="summary entry-title">PHP 5.2.9 released</h3>
