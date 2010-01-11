3b061b9f3e76de39b4036ef28d748b2c48e6a121
Pierre Joye <pajoye@php.net>
2010-01-11 09:10:53 +0000

    - die ie6

--- templates/footer.php
+++ templates/footer.php
@@ -27,7 +27,16 @@
                 </div><!-- .info -->
                 
                 <span class="corners-bottom"><span></span></span>
-            </li><!--#footer-->
-        </ul><!--#root-->
+            </li>
+        </ul>
+<!--[if IE 6]>
+<script type="text/javascript"> 
+	/*Load jQuery if not already loaded*/ if(typeof jQuery == 'undefined'){ document.write("<script type=\"text/javascript\"   src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></"+"script>"); var __noconflict = true; } 
+	var IE6UPDATE_OPTIONS = {
+		icons_path: "http://static.ie6update.com/hosted/ie6update/images/"
+	}
+</script>
+<script type="text/javascript" src="http://static.ie6update.com/hosted/ie6update/ie6update.js"></script>
+<![endif]-->
     </body>
 </html>
