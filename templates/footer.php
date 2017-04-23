dfe2db52b730828c9e00e21bb8745dd6784b5dae
Sobak <msobaczewski@gmail.com>
2017-04-23 17:35:26 +0200

    Drop code for old IE versions

--- templates/footer.php
+++ templates/footer.php
@@ -29,14 +29,5 @@
                 <span class="corners-bottom"><span></span></span>
             </li>
         </ul>
-<!--[if IE 6]>
-<script type="text/javascript"> 
-	/*Load jQuery if not already loaded*/ if(typeof jQuery == 'undefined'){ document.write("<script type=\"text/javascript\"   src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></"+"script>"); var __noconflict = true; } 
-	var IE6UPDATE_OPTIONS = {
-		icons_path: "http://static.ie6update.com/hosted/ie6update/images/"
-	}
-</script>
-<script type="text/javascript" src="http://static.ie6update.com/hosted/ie6update/ie6update.js"></script>
-<![endif]-->
     </body>
 </html>
