b78ec783878ce62bd7c0971f29cd36f5ce6e6051
Sobak <msobaczewski@gmail.com>
2017-04-28 08:47:34 +0200

    Further simplification of footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,13 +2,9 @@
                 <span class="corners-top"><span></span></span>
 
                 <div class="info">
-                    <div id="copyright">
-                        <a href="http://www.php.net/copyright.php">Copyright &copy; 2001-2017 The PHP Group</a><br>All rights reserved.
-                    </div><!-- #copyright -->
-
-                    <div id="footer-menu">
-                    </div><!-- #footer-menu -->
-                </div><!-- .info -->
+                    <a href="http://www.php.net/copyright.php">Copyright &copy; 2001-<?php echo date('Y') ?> The PHP Group</a><br>
+                    All rights reserved.
+                </div>
                 
                 <span class="corners-bottom"><span></span></span>
             </li>
