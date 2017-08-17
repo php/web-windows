571c227bb8f7b3e83d7a2e5f4fdea3240fbc5747
Dac Chartrand <dac.chartrand@gmail.com>
2017-08-17 18:12:47 -0400

    Automatically generate new from php.net atom feed.

---
+++ docroot/news.php
@@ -0,0 +1,211 @@
+
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.22 Released</h3>
+    <p class='news-date'><time datetime='2017-08-03T12:00:00+00:00'>03 Aug 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.22. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.22 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.22">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.21 Released</h3>
+    <p class='news-date'><time datetime='2017-07-06T12:00:00+00:00'>06 Jul 2017</time></p>
+    <div>
+        
+     <p>The PHP development team announces the immediate availability of PHP
+     7.0.21. This is a security release. Several security bugs were fixed in
+     this release.
+     
+     All PHP 7.0 users are encouraged to upgrade to this version.</p>
+     
+     <p>For source downloads of PHP 7.0.21 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+     Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+     The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.21">ChangeLog</a>.
+     </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.2.0 Alpha 1 Released</h3>
+    <p class='news-date'><time datetime='2017-06-08T17:03:24+00:00'>08 Jun 2017</time></p>
+    <div>
+        
+     <p>The PHP development team announces the immediate availability of PHP 7.2.0 Alpha 1.
+     This release marks the beginning of the second minor release in the PHP 7.x series.
+     All users of PHP are encouraged to test this version carefully,
+     and report any bugs and incompatibilities in the 
+     <a href="https://bugs.php.net/">bug tracking system</a>.</p>
+
+     <p><strong>THIS IS A DEVELOPMENT PREVIEW - DO NOT USE IT IN PRODUCTION!</strong></p>
+
+     <p>For information on new features and other changes, you can read the
+     <a href="https://github.com/php/php-src/blob/php-7.2.0alpha1/NEWS">NEWS</a> file,
+     or the <a href="https://github.com/php/php-src/blob/php-7.2.0alpha1/UPGRADING">UPGRADING</a> file
+     for a complete list of upgrading notes. These files can also be found in the release archive.</p>
+
+     <p>For source downloads of PHP 7.2.0 Alpha 1 please visit the <a href="https://downloads.php.net/~pollita/">download</a> page,
+     Windows sources and binaries can be found on <a href="http://windows.php.net/qa/">windows.php.net/qa/</a>.</p>
+
+     <p>The second alpha will be released on the 22nd of June. You can also read the full list of planned releases on our
+     <a href="https://wiki.php.net/todo/php72#timetable">wiki</a>.</p>
+
+     <p>Thank you for helping us make PHP better.</p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.20 Released</h3>
+    <p class='news-date'><time datetime='2017-06-08T12:00:00+00:00'>08 Jun 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.20. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.20 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.20">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.19 Released</h3>
+    <p class='news-date'><time datetime='2017-05-11T12:00:00+00:00'>11 May 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.19. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.19 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.19">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.18 Released</h3>
+    <p class='news-date'><time datetime='2017-04-13T12:00:00+00:00'>13 Apr 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.18. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.18 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.18">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.17 Released</h3>
+    <p class='news-date'><time datetime='2017-03-16T12:00:00+00:00'>16 Mar 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.17. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.17 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.17">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.16 Released</h3>
+    <p class='news-date'><time datetime='2017-02-16T12:00:00+00:00'>16 Feb 2017</time></p>
+    <div>
+        
+      <p>The PHP development team announces the immediate availability of PHP
+      7.0.16. Several bugs have been fixed.
+     
+      All PHP 7.0 users are encouraged to upgrade to this version.
+      </p>
+     
+      <p>For source downloads of PHP 7.0.16 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+      Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+      The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.16">ChangeLog</a>.
+      </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.15 Released</h3>
+    <p class='news-date'><time datetime='2017-01-19T12:00:00+00:00'>19 Jan 2017</time></p>
+    <div>
+        
+     <p>The PHP development team announces the immediate availability of PHP
+     7.0.15. This is a security release. Several security bugs were fixed in
+     this release.
+     
+     All PHP 7.0 users are encouraged to upgrade to this version.</p>
+     
+     <p>For source downloads of PHP 7.0.15 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+     Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+     The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.15">ChangeLog</a>.
+     </p>
+        </div>
+</div><!-- .info -->
+
+        
+<div class="info entry">
+    <!-- .info -->
+    <h3 class="summary entry-title">PHP 7.0.14 Released</h3>
+    <p class='news-date'><time datetime='2016-12-08T12:00:00+00:00'>08 Dec 2016</time></p>
+    <div>
+        
+     <p>The PHP development team announces the immediate availability of PHP
+     7.0.14. This is a security release. Several security bugs were fixed in
+     this release.
+     
+     All PHP 7.0 users are encouraged to upgrade to this version.</p>
+     
+     <p>For source downloads of PHP 7.0.14 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
+     Windows source and binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+     The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-7.php#7.0.14">ChangeLog</a>.
+     </p>
+        </div>
+</div><!-- .info -->
+
+        
---
+++ script/generate_news_page.php
@@ -0,0 +1,83 @@
+<?php
+
+// ----------------------------------------------------------------------------
+// Config
+// ----------------------------------------------------------------------------
+
+$news_feed_url = 'http://php.net/feed.atom';
+$news_feed_categories = ['releases'];
+$max_news_feed_items = 10;
+$path_to_news_file = __DIR__ . '/../docroot/news.php';
+
+// ----------------------------------------------------------------------------
+// Get RSS
+// ----------------------------------------------------------------------------
+
+$buffer = '';
+$feed = file_get_contents($news_feed_url);
+$xml = @simplexml_load_string($feed);
+if (!$xml) {
+    exit('Error: Could load Atom/Rss feed' . PHP_EOL);
+}
+
+// ----------------------------------------------------------------------------
+// Extract news from RSS
+// ----------------------------------------------------------------------------
+
+$i = 0;
+foreach ($xml as $node) {
+    if ($i >= $max_news_feed_items) {
+        break;
+    }
+    // Example: <category term="releases" label="New PHP release"/>
+    if (!in_array($node->category['term'], $news_feed_categories)) {
+        continue;
+    }
+
+    // Get title, date, and HTML
+    if ($node->title) {
+        $title = '<h3 class="summary entry-title">' . $node->title . '</h3>';
+    }
+    if ($node->published) {
+        $time = strtotime($node->published);
+        $human_readable = date('d M Y', $time);
+        $for_tools = date(DATE_W3C, $time);
+        $date = "<p class='news-date'><time datetime='{$for_tools}'>{$human_readable}</time></p>";
+    }
+    if ($node->content->div && $node->content->div instanceof \SimpleXMLElement) {
+        $info = $node->content->div->asXml();
+        $info = preg_replace(['/<div.*?>/', '/<\/div>/'], '', $info);
+    }
+
+    // Append to buffer
+    if (isset($title, $date, $info)) {
+        ob_start();
+        ?>
+
+<div class="info entry">
+    <!-- .info -->
+    <?php echo $title . PHP_EOL; ?>
+    <?php echo $date . PHP_EOL; ?>
+    <div>
+        <?php echo $info ; ?>
+    </div>
+</div><!-- .info -->
+
+        <?php
+        $buffer .= ob_get_clean();
+        ++$i;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Write new to file
+// ----------------------------------------------------------------------------
+
+if (!empty($buffer)) {
+    // Write html to file
+    file_put_contents($path_to_news_file, $buffer);
+    exit("Ok!" . PHP_EOL);
+} else {
+    exit("Error: No news?" . PHP_EOL);
+}
+
