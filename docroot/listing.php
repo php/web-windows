b4d26727a07bd845349b7e42d7f5a21ebc768d2e
Anatol Belski <ab@php.net>
2017-11-17 16:08:30 +0100

    Remove dead labels

--- docroot/listing.php
+++ docroot/listing.php
@@ -62,10 +62,6 @@ $minor_order = array(
 	);
 
 $labels = array(
-			'nts-VC9-x86' => 'VC9 x86 Non Thread Safe',
-			'ts-VC9-x86'  => 'VC9 x86 Thread Safe',
-			'nts-VC9-x64' => 'VC9 x64 Non Thread Safe',
-			'ts-VC9-x64'  => 'VC9 x64 Thread Safe',
 			'nts-VC11-x86' => 'VC11 x86 Non Thread Safe',
 			'ts-VC11-x86'  => 'VC11 x86 Thread Safe',
 			'nts-VC11-x64' => 'VC11 x64 Non Thread Safe',
