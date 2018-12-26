3e2e3daba2ec998ebde30eef7581d8251c975d2f
Anatol Belski <ab@php.net>
2018-12-26 14:05:55 +0100

    Add NEWS about OpenSSL 1.1.1

--- docroot/index.php
+++ docroot/index.php
@@ -32,6 +32,18 @@ include TPL_PATH . 'news_line.php';
           <!-- .block -->
  		  <div class="info entry">
 			<!-- .info -->
+			<h3 class="summary entry-title">OpenSSL 1.1.1a</h3>
+      <?php news_date('26-Dec-2018') ?>
+			<div>
+			<p>The recent release candidates 7.2.14RC1 and 7.3.1RC1 use the latest OpenSSL 1.1.1 release branch.
+				Please help testing these release candidates with the regard to the OpenSSL compatibility and report bugs.
+				This concerns the OpenSSL extension itself as well as the extensions depending on OpenSSL like cURL.
+			</p>
+			</div>
+		  </div><!-- .info -->
+
+ 		  <div class="info entry">
+			<!-- .info -->
 			<h3 class="summary entry-title">The windows.php.net site uses SSL by default</h3>
       <?php news_date('12-Mar-2018') ?>
 			<div>
