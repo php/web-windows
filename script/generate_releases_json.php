0b225f21492c6f65abb20b76c440dc02920a14f7
Andi Rückauer <arueckauer@github.com>
2019-08-08 12:32:19 +0200

    Change date format of mtime in releaes.json to ISO 8601

--- script/generate_releases_json.php
+++ script/generate_releases_json.php
@@ -3,5 +3,32 @@
 require_once __DIR__ . '/../include/config.php';
 require_once __DIR__ . '/../include/listing.php';
 
-$releases = generate_listing('downloads/releases', MODE_RELEASE);
+$releases = generate_listing('downloads/releases', MODE_RELEASE, 'c');
+
+/*
+ * Change date format to ISO 8601
+ * Altering date format in generate_listing() could break third-party scrapers
+ */
+$timezone = new DateTimeZone('-07:00');
+foreach ($releases as &$release) {
+    foreach ($release as &$flavour) {
+        if (! is_array($flavour) || ! isset($flavour['mtime'])) {
+            continue;
+        }
+
+        try {
+            $date = new DateTimeImmutable($flavour['mtime'], $timezone);
+            $flavour['mtime'] = $date->format('c');
+        } catch (Exception $exception) {
+            printf(
+                'An error occurred while trying to format date "%s": %s',
+                $flavour['mtime'],
+                $exception->getMessage()
+            );
+        }
+    }
+    unset($flavour);
+}
+unset($release);
+
 file_put_contents(RELEASES_DIR . 'releases.json', json_encode($releases));
