24bb6ed975b19511242038b60beaf8f3993443fe
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-06 17:47:53 +0200

    Remove Windows Defender note
    This issue appears to have been resolved; there where no reports for
    many months.

--- templates/news_line.php
+++ templates/news_line.php
@@ -1,6 +1,2 @@
-            <li id="news-line" style="line-height: 2em;"><!-- #news-line -->
-            <p>
-              Are you seeing a warning from Windows Defender? Check out <a
-              href="https://windows.php.net/#news-2020-08-07-1">this info</a>.
-            </p>
+            <li id="news-line"><!-- #news-line -->
             </li><!-- #news-line -->
