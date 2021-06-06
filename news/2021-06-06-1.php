44c0052dc2129882031678f7b27f3efd57aa3fc0
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-06 22:39:14 +0200

    Post about PHP 8 snapshot builds

---
+++ news/2021-06-06-1.php
@@ -0,0 +1,18 @@
+<div class="info entry" id="news-2020-08-09-1">
+  <!-- .info -->
+  <h3 class="summary entry-title">PHP 8 Snapshot Builds</h3>
+  <?php news_date('06-Jun-2021') ?>
+  <div>
+    <p>
+      You may have already noticed that we didn't provide snapshot builds for
+      PHP 8 for many months. There are no plans to change that, but users can
+      get <a href="https://github.com/shivammathur/php-builder-windows">third
+      party privided nighly builds</a>.
+    </p>
+    <p>
+      We will, of course, continue to provide Windows builds of all PHP
+      (pre-)releases. So watch out for the release of PHP 8.1.0alpha1, scheduled
+      for 10 Jun 2021!
+    </p>
+  </div>
+</div><!-- .info -->
