c29e0e8bc257c409436840170e944238b78ff9b2
Christoph M. Becker <cmbecker69@gmx.de>
2021-12-03 00:52:43 +0100

    [NEWS] PECL DLLs for PHP 8.1

---
+++ news/2021-12-02-1.php
@@ -0,0 +1,28 @@
+<div class="info entry" id="news-2021-12-02-1">
+  <!-- .info -->
+  <h3 class="summary entry-title">PECL DLLs for PHP 8.1</h3>
+  <?php news_date('02-Dec-2021') ?>
+  <div>
+    <p>
+      A week ago <a href="https://www.php.net/archive/2021.php#2021-11-25-1">PHP 8.1.0 has been released</a>
+      and you may be eager to try it out, but you need one or more PECL packages
+      for which there are no PHP 8.1 binary packages available yet, and you may wonder when
+      these will be made available. Well, the PHP for Windows team does not want to
+      provide possibly premature binary packages, so as usual, we will wait until
+      late January or early February to trigger the mass build for all applicable
+      PECL packages; that is, after PHP 8.1.2 has been released, what is currently
+      scheduled for January, 20th. After the builds have been done, you can
+      download the binary packages from <a href="https://pecl.php.net/">pecl.php.net</a>
+      as usual.
+    </p>
+    <p>
+      Waiting for two months is no option for you? In that case you can download
+      <a href="https://windows.php.net/downloads/pecl/snaps/">snapshot builds</a> of
+      some of the PECL packages. If there is no snapshot for your package available,
+      consider to <a href="https://windows.php.net/team.php">contact the team</a>
+      and to request that a snapshot for that package is being build. Note though,
+      that these snapshot builds are not recommended for production. However, testing
+      these and reporting issues is very welcome!
+    </p>
+  </div>
+</div><!-- .info -->
