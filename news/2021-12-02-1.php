bbbd7914f8f1e556f8ade92f4a32b39349337faa
Christoph M. Becker <cmbecker69@gmx.de>
2022-10-17 15:40:21 +0200

    Fix GH-26: Mention lack of PECL DLLs
    Better late than never.

--- news/2021-12-02-1.php
+++ news/2021-12-02-1.php
@@ -1,28 +1,13 @@
-<div class="info entry" id="news-2021-12-02-1">
+<div class="info entry" id="news-2022-10-17-1">
   <!-- .info -->
-  <h3 class="summary entry-title">PECL DLLs for PHP 8.1</h3>
-  <?php news_date('02-Dec-2021') ?>
+  <h3 class="summary entry-title">Missing PECL DLLs</h3>
+  <?php news_date('17-Oct-2022') ?>
   <div>
     <p>
-      A week ago <a href="https://www.php.net/archive/2021.php#2021-11-25-1">PHP 8.1.0 has been released</a>
-      and you may be eager to try it out, but you need one or more PECL packages
-      for which there are no PHP 8.1 binary packages available yet, and you may wonder when
-      these will be made available. Well, the PHP for Windows team does not want to
-      provide possibly premature binary packages, so as usual, we will wait until
-      late January or early February to trigger the mass build for all applicable
-      PECL packages; that is, after PHP 8.1.2 has been released, what is currently
-      scheduled for January, 20th. After the builds have been done, you can
-      download the binary packages from <a href="https://pecl.php.net/">pecl.php.net</a>
-      as usual.
-    </p>
-    <p>
-      Waiting for two months is no option for you? In that case you can download
-      <a href="https://windows.php.net/downloads/pecl/snaps/">snapshot builds</a> of
-      some of the PECL packages. If there is no snapshot for your package available,
-      consider to <a href="https://windows.php.net/team.php">contact the team</a>
-      and to request that a snapshot for that package is being build. Note though,
-      that these snapshot builds are not recommended for production. However, testing
-      these and reporting issues is very welcome!
-    </p>
+      You may have already noticed that DLLs for new PECL releases are no longer
+      available for several months. This is because the Windows PECL build machine
+      died, and the team is still working on the long term plan of building DLLs for
+      PECL extensions with a new CI process. We're doing our best to finish that as
+      soon as possible, and keep you up to date.
   </div>
 </div><!-- .info -->
