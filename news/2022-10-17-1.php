f974845b186551866ac34665279cd39bcb6f58ff
Christoph M. Becker <cmbecker69@gmx.de>
2022-10-17 15:43:18 +0200

    Fix the news
    The previous commit overwrote the latest news entry instead of adding a
    new one.

---
+++ news/2022-10-17-1.php
@@ -0,0 +1,13 @@
+<div class="info entry" id="news-2022-10-17-1">
+  <!-- .info -->
+  <h3 class="summary entry-title">Missing PECL DLLs</h3>
+  <?php news_date('17-Oct-2022') ?>
+  <div>
+    <p>
+      You may have already noticed that DLLs for new PECL releases are no longer
+      available for several months. This is because the Windows PECL build machine
+      died, and the team is still working on the long term plan of building DLLs for
+      PECL extensions with a new CI process. We're doing our best to finish that as
+      soon as possible, and keep you up to date.
+  </div>
+</div><!-- .info -->
