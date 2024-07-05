401961ee0b2446b069e96fa95af907bad92914bc
Christoph M. Becker <cmbecker69@gmx.de>
2024-07-05 17:46:20 +0200

    Show PHP 8.4 (pre-)releases on the Website (#27)
    * Show PHP 8.4 (pre-)releases on the Website
    
    According to a quick discussion[1], these will be build with VS17 (aka.
    Visual Studio 2022), so we also update the respective info in the
    sidebar.
    
    We also add an announcement regarding the update to Visual Studio 2022.
    
    [1] <https://github.com/php/php-sdk-binary-tools/pull/5#issuecomment-2206216181>

---
+++ news/2024-07-04-1.php
@@ -0,0 +1,15 @@
+<div class="info entry" id="news-2024-07-04-1">
+  <!-- .info -->
+  <h3 class="summary entry-title">PHP 8.4 builds use Visual Studio 2022</h3>
+  <?php news_date('04-Jul-2024') ?>
+  <div>
+    <p>
+      While previous minor versions of PHP 8 have been built with Visual Studio
+      2019 (aka. VS16), it is time to use Visual Studio 2022 (aka. VS17) for the
+      PHP 8.4 builds. To use these builds, you may need to install or update the
+      Visual C++ Redistributable for Visual Studio 2015-2022
+      <a href="https://aka.ms/vs/17/release/vc_redist.x64.exe">x64</a> or
+      <a href="https://aka.ms/vs/17/release/vc_redist.x86.exe">x86</a>.
+    </p>
+  </div>
+</div><!-- .info -->
